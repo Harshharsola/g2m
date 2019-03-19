2961cc0beaffe7641d52b55ba4832285bff6625a
Harsh Harsola <harshharsola@gmail.com>
2019-03-19 23:50:43 +0530

    Update BiddingPortal.js

--- src/BiddingPortal.js
+++ src/BiddingPortal.js
@@ -1,31 +1,56 @@
 import React, { Component } from 'react'
 import './biddingportal.css'
 export default class BiddingPortal extends Component {
-    constructor(props)
-    {
+    constructor(props) {
         super(props);
+        this.state = {
+            quotationValue: '',
+            Price: ''
+        }
+
     }
+
+    postFormData = async function (e) {
+        e.preventDefault();
+    };
+
     render() {
-        return <div className="bidding-portal">
-            <div className="name-field">
-                Enter your Name</div><input className="name-class"></input>
+        return (
+            <form onSubmit={this.postFormData} id="bidding-portal-form" className="bidding-portal">
+                <div className="name-field">
+                    Enter your Name</div><input className="name-class"></input>
 
-            <div className="contact-field">
-                Enter your Phone Number</div><input className="contact-detail"></input>
+                <div className="contact-field">
+                    Enter your Phone Number</div><input className="contact-detail"></input>
 
-            <div className="summary-field">Enter Quotation Here
-            <textarea className="quotation-summary" placeholder="Submit A Detailed Overview Of Your Quotation" ></textarea>
-            </div>
-            <div className="price-field">
-                Enter Your Final Price</div><input className="final-price"></input>
-            <div className="submit-class">
-            <button className="submit">Submit</button>
-            </div>
-            
-
-        </div>
+                <div className="summary-field">Enter Quotation Here
+                <textarea value={this.state.quotationValue} onChange={evt => this.updateQuotationValue(evt)} className="quotation-summary" placeholder="Submit A Detailed Overview Of Your Quotation" ></textarea>
+                </div>
+                <div className="price-field">
+                    Enter Your Final Price</div>
+                <input value = {this.state.Price} onChange = {pvt => this.updatePrice(pvt)} className="final-price"></input>
+                <div className="submit-class">
+                    <input type="submit" className="submit" name="Submit" />
+                </div>
+            </form>
+        )
 
 
 
     }
+
+    updateQuotationValue(evt) {
+        this.setState({
+            quotationValue: evt.target.value,
+        });
+        console.log("quotation:",evt.target.value);
+    }
+    updatePrice(pvt) {
+        this.setState({
+            Price : pvt.target.value
+        });
+        console.log("price:",this.state.Price);
+
+    }
+
 }
