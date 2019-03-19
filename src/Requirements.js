0d9683359d7c621c8cd77d9f3f6cccd6a1896c0c
Harsh Harsola <harshharsola@gmail.com>
2019-03-20 01:43:01 +0530

    Update Requirements.js

--- src/Requirements.js
+++ src/Requirements.js
@@ -1,33 +1,73 @@
 import React, { Component } from 'react'
 import './Requirements.css'
+let i=0;
+
+
 export default class Requirements extends Component {
     constructor(props) {
         super(props);
-        this.state = { input: [] };
+        this.state = { 
+            input: [],
+            ProductName:[],
+            Quantity: []
+        };
+ 
+        
     }
-
-
     render() {
-        function render_input() {
+        function render_input(index) {
             return (<div>
-                < input className="Product-Name" placeholder="Enter Product Name">
+                < input  onChange = {evt =>{evt.persist(); this.updateProduct(evt, index)}} className="Product-Name" placeholder="Enter Product Name">
                 </input>
-                <input className="quantity" placeholder="Enter Quantity Here">
+                <input  onChange = {pvt => {pvt.persist(); this.updateQuantity(pvt, index)}} className="quantity" placeholder="Enter Quantity Here">
                 </input>
             </div>);
 
         }
-        return <div className="requirement">
+        return (<div className="requirement">
             Enter your requirements
             {this.state.input}
-        <button className="add" onClick={() => { this.setState((state) => { return { input: [...state.input, render_input()] } }) }}>Add another field+</button>
+        <button className="add" onClick={() => { this.setState((state) => { return { input: [...state.input, render_input.bind(this)(state.input.length)] } }) }}>Add another field+</button>
             <div className="inputGroup">
             </div>
             <button className="Submit"> submit</button>
 
         </div>
+        );
+    }//samajh nhi aaya mujhe kya bolna h=c
+    //Mei samajh gaya. Ham function kal karahe hen, jisme this binded nahi he.
+    updateProduct(evt, index){
+        console.log(this.state.ProductName);
+        this.setState((state)=>{
+            let newProductName = [...state.ProductName];
+            if(index>newProductName.length)
+            {
+                newProductName.push(evt.target.value);
+            }
 
+            else{
+                newProductName[index] = evt.target.value;
+            }
+            return {
+            ProductName : newProductName
+        }});
+        // console.log(ProductName);
     }
+    updateQuantity(pvt, index){
+        this.setState((state)=>{
+            let newQuantityName = [...state.Quantity];
+            if(index>newQuantityName.length)
+            {
+                newQuantityName.push(pvt.target.value);
+            }
 
+            else{
+                newQuantityName[index] = pvt.target.value;
+            }
+            return {
+            Quantity : newQuantityName
+        }});
+        }
+    
     //Find why html is on top
 }
